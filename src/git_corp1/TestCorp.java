0806a70c0b1e9fe6b7fbf28c4d6923d0e88a9b5d
t427795737 <t427795737@users.noreply.github.com>
2014-12-17 10:44:51 +0800

    testcopr2
    Signed-off-by: t427795737 <tianof427795737@126.com>

---
+++ src/git_corp1/TestCorp.java
@@ -0,0 +1,40 @@
+/**  
+* @Title:  TestCorp.java
+* @Package git_corp1
+* @Description: TODO(用一句话描述该文件做什么)
+* @author wangxiaoming
+* @date  Dec 17, 2014 2:09:53 PM
+* @version V1.0  
+* Update Logs:
+* ****************************************************
+* Name:
+* Date:
+* Description:
+******************************************************
+*/
+package git_corp1;
+
+/**
+ * @ClassName: TestCorp
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author wangxiaoming
+ * @date Dec 17, 2014 2:09:53 PM
+ *
+ */
+public class TestCorp
+{
+
+    /**
+     * @Title: main
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author wangxiaoming
+     * @param args
+     * @throws
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("son of bitch");
+    }
+
+}
